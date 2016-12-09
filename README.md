80c8b628cd7bd736c014feef72975d5e53b4993a
techgaun <coolsamar207@gmail.com>
2016-12-09 14:09:59 -0600

    update documentations

--- README.md
+++ README.md
@@ -1,23 +1,81 @@
 # xmart_things
 
-> Elixir client for Smart Things
+> SmartThings OAuth2 Strategy and Client for Elixir
+
+This was implemented while I followed [this](http://docs.smartthings.com/en/latest/smartapp-web-services-developers-guide/overview.html) but can be used for all the needs to build Web Services SmartApps with SmartThings.
 
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
 
-  1. Add `xmart_things` to your list of dependencies in `mix.exs`:
+- Add `xmart_things` to your list of dependencies in `mix.exs`:
 
-    ```elixir
-    def deps do
-      [{:xmart_things, "~> 0.1.0"}]
-    end
-    ```
+```elixir
+def deps do
+  [{:xmart_things, "~> 0.1.0"}]
+end
+```
 
-  2. Ensure `xmart_things` is started before your application:
+- Ensure `xmart_things` is started before your application:
 
-    ```elixir
-    def application do
-      [applications: [:xmart_things]]
-    end
-    ```
+```elixir
+def application do
+  [applications: [:xmart_things]]
+end
+```
+
+## Configuration
+
+Add a configuration block as below in your configuration:
+
+```elixir
+config :xmart_things,
+  client_id: System.get_env("ST_CLIENT_ID"),
+  client_secret: System.get_env("ST_CLIENT_SECRET"),
+  redirect_uri: System.get_env("ST_REDIRECT_URI"),
+  scope: "app"
+  # , app_uuid: "" # set this if you wish to explicitly specify site/smartapp base uri to call
+```
+
+## Usage
+
+```elixir
+# Generate the authorization to redirect client for authorization
+XmartThings.authorize_url!
+
+# Capture the `code` from redirect in your callback handler route
+st_client = XmartThings.get_token!(code: code)
+
+# Use the access token to access resources
+
+locks = XmartThings.get!(st_client, "/locks").body
+
+# or
+case XmartThings.get(st_client, "/locks") do
+  {:ok, %OAuth2.Response{status_code: 401, body: body}} ->
+    {:error, "unauthorized token"}
+  {:ok, %OAuth2.Response{status_code: status_code, body: locks}} when status_code in [200..399] ->
+    locks
+  {:error, %OAuth2.Error{reason: reason}} ->
+    {:error, reason}
+end
+
+# Use `endpoints/1` or `endpoints!/1` to get the list of endpoints that you can use to talk with SmartApp
+XmartThings.endpoints!(st_client)
+
+# It can return bunch of responses. Pick one of those (usually the first one) to perform your requests to SmartApp
+
+[%{"uri" => uri} | _] = XmartThings.endpoints!(st_client).body
+
+# and now send the requests to URLs like below:
+
+XmartThings.get(%{st_client | site: uri}, "/locks")
+
+# if you set `app_uuid` on your config, you don't need to update `site` in the `st_client` struct
+
+XmartThings.get(st_client, "/locks")
+```
+
+## Author
+
+- [techgaun](https://github.com/techgaun)
--- lib/xmart_things.ex
+++ lib/xmart_things.ex
@@ -11,8 +11,10 @@ defmodule XmartThings do
       st_client = XmartThings.get_token!(code: code)
 
       # Use the access token to access resources
+
       locks = XmartThings.get!(st_client, "/locks").body
 
+      # or
       case XmartThings.get(st_client, "/locks") do
         {:ok, %OAuth2.Response{status_code: 401, body: body}} ->
           {:error, "unauthorized token"}
@@ -21,29 +23,50 @@ defmodule XmartThings do
         {:error, %OAuth2.Error{reason: reason}} ->
           {:error, reason}
       end
+
+      # Use `endpoints/1` or `endpoints!/1` to get the list of endpoints that you can use to talk with SmartApp
+      XmartThings.endpoints!(st_client)
+
+      # It can return bunch of responses. Pick one of those (usually the first one) to perform your requests to SmartApp
+
+      [%{"uri" => uri} | _] = XmartThings.endpoints!(st_client).body
+
+      # and now send the requests to URLs like below:
+
+      XmartThings.get(%{st_client | site: uri}, "/locks")
   """
 
   alias OAuth2.Strategy.AuthCode
   use OAuth2.Strategy
 
   @endpoint_uri "https://graph.api.smartthings.com/api/smartapps/endpoints"
+  @default_site "https://graph.api.smartthings.com"
 
+  @doc """
+  Creates a OAuth client struct for SmartThings Authorization
+  """
   def client do
     OAuth2.Client.new([
       strategy: __MODULE__,
       client_id: client_id,
       client_secret: client_secret,
       redirect_uri: redirect_uri,
-      site: "https://graph.api.smartthings.com",
+      site: site,
       authorize_url: "https://graph.api.smartthings.com/oauth/authorize",
       token_url: "https://graph.api.smartthings.com/oauth/token"
     ])
   end
 
+  @doc """
+  Creates authorization URL based on the `client` configuration
+  """
   def authorize_url! do
     OAuth2.Client.authorize_url!(client, scope: scope)
   end
 
+  @doc """
+  Fetches an OAuth2.AccessToken struct by making a request to the token endpoint.
+  """
   def get_token!(params \\ [], headers \\ [], opts \\ []) do
     OAuth2.Client.get_token!(client(), params, headers, opts)
   end
@@ -59,9 +82,22 @@ defmodule XmartThings do
     |> AuthCode.get_token(params, headers)
   end
 
+  @doc """
+  Retrieve the lists of endpoints exposed for a SmartApp
+  """
   def endpoints(client), do: client |> __MODULE__.get(@endpoint_uri)
+
+  @doc """
+  same as endpoints/1 but raises error
+
+  ## Example getting URI for further requests
+      [%{"uri" => uri} | _] = XmartThings.endpoints!(st_client).body
+  """
   def endpoints!(client), do: client |> __MODULE__.get!(@endpoint_uri)
 
+  @doc """
+  Use these to send GET requests to the endpoints via `OAuth2.Client`.
+  """
   defdelegate get!(client, endpoint), to: OAuth2.Client
   defdelegate get!(client, endpoint, headers), to: OAuth2.Client
   defdelegate get!(client, endpoint, headers, params), to: OAuth2.Client
@@ -69,10 +105,49 @@ defmodule XmartThings do
   defdelegate get(client, endpoint, headers), to: OAuth2.Client
   defdelegate get(client, endpoint, headers, params), to: OAuth2.Client
 
+  @doc """
+  Use these to send PUT requests to the endpoints via `OAuth2.Client`.
+  """
+  defdelegate put!(client, endpoint), to: OAuth2.Client
+  defdelegate put!(client, endpoint, body), to: OAuth2.Client
+  defdelegate put!(client, endpoint, body, headers), to: OAuth2.Client
+  defdelegate put!(client, endpoint, body, headers, opts), to: OAuth2.Client
+  defdelegate put(client, endpoint), to: OAuth2.Client
+  defdelegate put(client, endpoint, body), to: OAuth2.Client
+  defdelegate put(client, endpoint, body, headers), to: OAuth2.Client
+  defdelegate put(client, endpoint, body, headers, opts), to: OAuth2.Client
+
+  @doc """
+  Use these to send POST requests to the endpoints via `OAuth2.Client`.
+  """
+  defdelegate post!(client, endpoint), to: OAuth2.Client
+  defdelegate post!(client, endpoint, body), to: OAuth2.Client
+  defdelegate post!(client, endpoint, body, headers), to: OAuth2.Client
+  defdelegate post!(client, endpoint, body, headers, opts), to: OAuth2.Client
+  defdelegate post(client, endpoint), to: OAuth2.Client
+  defdelegate post(client, endpoint, body), to: OAuth2.Client
+  defdelegate post(client, endpoint, body, headers), to: OAuth2.Client
+  defdelegate post(client, endpoint, body, headers, opts), to: OAuth2.Client
+
+  @doc """
+  Use these to send DELETE requests to the endpoints via `OAuth2.Client`.
+  """
+  defdelegate delete!(client, endpoint), to: OAuth2.Client
+  defdelegate delete!(client, endpoint, body), to: OAuth2.Client
+  defdelegate delete!(client, endpoint, body, headers), to: OAuth2.Client
+  defdelegate delete!(client, endpoint, body, headers, opts), to: OAuth2.Client
+  defdelegate delete(client, endpoint), to: OAuth2.Client
+  defdelegate delete(client, endpoint, body), to: OAuth2.Client
+  defdelegate delete(client, endpoint, body, headers), to: OAuth2.Client
+  defdelegate delete(client, endpoint, body, headers, opts), to: OAuth2.Client
+
   defp client_id, do: Application.get_env(:xmart_things, :client_id)
   defp client_secret, do: Application.get_env(:xmart_things, :client_secret)
   defp redirect_uri, do: Application.get_env(:xmart_things, :redirect_uri)
   defp display_link, do: Application.get_env(:xmart_things, :display_link)
   defp display_name, do: Application.get_env(:xmart_things, :display_name)
   defp scope, do: Application.get_env(:xmart_things, :scope) || "app"
+  defp site, do: :xmart_things |> Application.get_env(:app_uuid) |> _site
+  defp _site(uuid) when is_binary(uuid), do: @default_site <> "/api/smartapps/installations/" <> uuid
+  defp _site(_), do: @default_site
 end
